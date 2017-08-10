e336155cba8cb61eb48b01591b207da58409c4b3
Marcel Bloemendaal <m.bloemendaal@yipyip.nl>
2017-08-10 15:27:58 +0200

    Reverted a Swift 4 only API

--- SwiftStylable/Classes/Style/Style.swift
+++ SwiftStylable/Classes/Style/Style.swift
@@ -322,25 +322,25 @@ open class Style {
 					return UIFont.italicSystemFont(ofSize: size)
 					
 				case "thinSystemFont":
-					return UIFont.systemFont(ofSize: size, weight: UIFont.Weight.thin)
+					return UIFont.systemFont(ofSize: size, weight: UIFontWeightThin)
 					
 				case "blackSystemFont":
-					return UIFont.systemFont(ofSize: size, weight: UIFont.Weight.black)
+					return UIFont.systemFont(ofSize: size, weight: UIFontWeightBlack)
 					
 				case "heavySystemFont":
-					return UIFont.systemFont(ofSize: size, weight: UIFont.Weight.heavy)
+					return UIFont.systemFont(ofSize: size, weight: UIFontWeightHeavy)
 					
 				case "lightSystemFont":
-					return UIFont.systemFont(ofSize: size, weight: UIFont.Weight.light)
+					return UIFont.systemFont(ofSize: size, weight: UIFontWeightLight)
 					
 				case "mediumSystemFont":
-					return UIFont.systemFont(ofSize: size, weight: UIFont.Weight.medium)
+					return UIFont.systemFont(ofSize: size, weight: UIFontWeightMedium)
 					
 				case "semiboldSystemFont":
-					return UIFont.systemFont(ofSize: size, weight: UIFont.Weight.semibold)
+					return UIFont.systemFont(ofSize: size, weight: UIFontWeightSemibold)
 					
 				case "ultraLightSystemFont":
-					return UIFont.systemFont(ofSize: size, weight: UIFont.Weight.ultraLight)
+					return UIFont.systemFont(ofSize: size, weight: UIFontWeightUltraLight)
 					
 				default:
 					return UIFont(name: name, size: size)
